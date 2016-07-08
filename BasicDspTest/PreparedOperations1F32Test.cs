8ca3698d8cefc69a95d7fc600f6d923d9efd4d76
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-07-07 22:31:29 -0700

    Added a new test case for PreparedOps1F32

---
+++ BasicDspTest/PreparedOperations1F32Test.cs
@@ -0,0 +1,24 @@
+﻿using BasicDsp;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicDspTest
+{
+    [TestClass]
+    public class PreparedOperations1F32Test
+    {
+        [TestMethod]
+        public void PrepareOpsAndExecuteOnVector()
+        {
+            using (var ops = new PreparedOps1F32())
+            using (var vector = DataVector32.NewRealTimeVectorFromConstant(2, 10))
+            {
+                ops.AddOps((v) => 
+                v.AddReal(1.0f)
+                 .MultiplyReal(3.0f));
+                ops.Exec((DataVector32)vector);
+                vector[0].Should().Be(9.0f);
+            }
+        }
+    }
+}
